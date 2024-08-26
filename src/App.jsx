3e952833e51582766c61ad44df0f48692ae6fad9
AmrHesham <msam.team20@gmail.com>
2024-08-26 13:49:26 +0300

    Test Tag

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,14 @@ import AboutSection from './components/about-section/AboutSection';
 import Testimoniels from './components/testimoniels/Testimoniels';
 
 function App() {
+  window.dataLayer.push({
+    event:'pageview',
+    page:{
+      url:'http://localhost:5173/',
+      title:'Home'
+    }
+  })
+  console.log(window.dataLayer)
   return (
     
      <>
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import tagManager from 'react-gtm-module'
 import App from './App.jsx'
 import './index.css'
 
+const tagManagerArgs={
+  gtmId:'GTM-W9KD3PHX'
+}
+tagManager.initialize(tagManagerArgs)
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
